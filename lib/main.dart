a698e9d0a7ad8807585cd2dd459049ac51aa8c5e
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2021-12-20 10:54:33 +0530

    Styled authentication screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,29 @@ import 'package:flutter/material.dart';
 
 import './screens/auth_screen.dart';
 import './screens/chat_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chat Bit',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        backgroundColor: Colors.indigo,
+        accentColor: Colors.lightBlue,
+        accentColorBrightness: Brightness.dark,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.indigo,
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
       ),
       home: AuthScreen(),
     );
