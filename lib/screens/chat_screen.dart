cea46aa5754392accd047a9a1b3e86184166c3f2
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2021-12-20 10:30:43 +0530

    Configured initial adding messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -27,7 +27,11 @@ class ChatScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/oZOF1M3giWMSVvxozXai/messages')
+              .add({'text': 'This was added by clicking the button!'});
+        },
       ),
     );
   }
