c366b1c0373c4b89c8a3a152eb6ef98ae5f028ce
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2021-12-21 19:36:34 +0530

    Added validators for Text form fields

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -19,15 +19,33 @@ class _AuthFormState extends State<AuthForm> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address!';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email address',
                     ),
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 4) {
+                        return 'Username must be atleast 4 characters long.';
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(labelText: 'Username'),
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be atleast 7 characters long.'  ;
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(labelText: 'Password'),
                     obscureText: true,
                   ),
