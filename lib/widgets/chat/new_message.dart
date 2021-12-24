7af39bc64548a16d5c99eae8b378d3375d3ec900
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2021-12-24 10:01:18 +0530

    Ordered chat messages correctly

--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NewMessage extends StatefulWidget {
   @override
@@ -6,7 +7,17 @@ class NewMessage extends StatefulWidget {
 }
 
 class _NewMessageState extends State<NewMessage> {
+  final _controller = new TextEditingController();
   var _enteredMessage = '';
+
+  void _sendMessage() {
+    FocusScope.of(context).unfocus();
+    Firestore.instance.collection('chat').add({
+      'text': _enteredMessage,
+      'createdAt': Timestamp.now(),
+    });
+    _controller.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +28,7 @@ class _NewMessageState extends State<NewMessage> {
         children: [
           Expanded(
             child: TextField(
+              controller: _controller,
               decoration: InputDecoration(labelText: 'Send a message...'),
               onChanged: (value) {
                 setState(() {
@@ -28,7 +40,7 @@ class _NewMessageState extends State<NewMessage> {
           IconButton(
             color: Theme.of(context).primaryColor,
             icon: Icon(Icons.send),
-            onPressed: _enteredMessage.trim().isEmpty ? null : () {},
+            onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
           ),
         ],
       ),
