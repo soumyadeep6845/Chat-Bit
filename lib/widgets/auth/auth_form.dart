c33921b9663c152fbdb8dfaae2b0145ae47b8698
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2021-12-25 11:14:44 +0530

    Added image upload functionality

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -57,7 +57,7 @@ class _AuthFormState extends State<AuthForm> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  UserImagePicker(),
+                  if (!_isLogin) UserImagePicker(),
                   TextFormField(
                     key: ValueKey('email'),
                     validator: (value) {
--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   @override
@@ -6,16 +9,29 @@ class UserImagePicker extends StatefulWidget {
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+
+  void _pickImage() async {
+    final picker = ImagePicker();
+    final pickImage = await picker.getImage(source: ImageSource.camera);
+    final pickedImageFile = await File(pickImage.path);
+    setState(() {
+      _pickedImage = pickedImageFile;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         CircleAvatar(
           radius: 40,
+          backgroundColor: Colors.grey,
+          backgroundImage: _pickedImage != null ? FileImage(_pickedImage) : null,
         ),
         FlatButton.icon(
           textColor: Theme.of(context).primaryColor,
-          onPressed: () {},
+          onPressed: _pickImage,
           icon: Icon(Icons.image),
           label: Text('Add image'),
         ),
