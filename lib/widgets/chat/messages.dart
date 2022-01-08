c17c63d0c24cd0d830cd0622e87506ee36115afa
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2022-01-08 11:19:00 +0530

    Upgraded to latest packages

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -7,16 +7,9 @@ import '/widgets/chat/message_bubble.dart';
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-            future: FirebaseAuth.instance.currentUser(),
-            builder: (ctx, futureSnapshot) {
-              if (futureSnapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              return StreamBuilder(
-      stream: Firestore.instance
+    final user = FirebaseAuth.instance.currentUser;
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
           .collection('chat')
           .orderBy(
             'createdAt',
@@ -25,23 +18,22 @@ class Messages extends StatelessWidget {
           .snapshots(),
       builder: (ctx, chatSnapshot) {
         if (chatSnapshot.connectionState == ConnectionState.waiting) {
-          return Center(
+          return const Center(
             child: CircularProgressIndicator(),
           );
         }
-        final chatDocs = chatSnapshot.data.documents;
+        final chatDocs = chatSnapshot.data.docs;
         return ListView.builder(
-                reverse: true,
-                itemCount: chatDocs.length,
-                itemBuilder: (ctx, index) => MessageBubble(
-                  chatDocs[index]['text'],
-                  chatDocs[index]['username'],
-                  chatDocs[index]['userImage'],
-                  chatDocs[index]['userId'] == futureSnapshot.data.uid,
-                  key: ValueKey(chatDocs[index].documentID),
-                ),
-              );
-            });
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (ctx, index) => MessageBubble(
+            chatDocs[index].data()['text'],
+            chatDocs[index].data()['username'],
+            chatDocs[index].data()['userImage'],
+            chatDocs[index].data()['userId'] == user.uid,
+            key: ValueKey(chatDocs[index].id),
+          ),
+        );
       },
     );
   }
--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -13,14 +13,14 @@ class _NewMessageState extends State<NewMessage> {
 
   void _sendMessage() async {
     FocusScope.of(context).unfocus();
-    final user = await FirebaseAuth.instance.currentUser();
-    final userData = await Firestore.instance.collection('users').document(user.uid).get();
-    Firestore.instance.collection('chat').add({
+    final user = FirebaseAuth.instance.currentUser;
+    final userData = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+    FirebaseFirestore.instance.collection('chat').add({
       'text': _enteredMessage,
       'createdAt': Timestamp.now(),
       'userId': user.uid,
-      'username': userData['username'],
-      'userImage': userData['image_url'],
+      'username': userData.data()['username'],
+      'userImage': userData.data()['image_url'],
     });
     _controller.clear();
   }
@@ -38,7 +38,7 @@ class _NewMessageState extends State<NewMessage> {
               autocorrect: true,
               enableSuggestions: true,
               controller: _controller,
-              decoration: InputDecoration(labelText: 'Send a message...'),
+              decoration: const InputDecoration(labelText: 'Send a message...'),
               onChanged: (value) {
                 setState(() {
                   _enteredMessage = value;
@@ -48,7 +48,7 @@ class _NewMessageState extends State<NewMessage> {
           ),
           IconButton(
             color: Theme.of(context).primaryColor,
-            icon: Icon(Icons.send),
+            icon: const Icon(Icons.send),
             onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
           ),
         ],
